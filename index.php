d4ad7f7b440ebbe40eefa6444e8414805c9563a1
juniormt10 <junior.emt10@gmail.com>
2023-09-15 10:39:03 -0500

    1era version

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "./inc/head.php" ?>
+</head>
+<body>
+    
+
+<footer>
+    <div>
+        
+        <hr>
+        <h1>Desarrollado por Morales Tarazona Junior Eutemio</h1>
+    </div>
+</footer>
+</body>
+</html>
